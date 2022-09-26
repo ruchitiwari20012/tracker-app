cf9489c633af39aca46331098ed15a55b2f93c98
ruchitiwari20012 <67836089+ruchitiwari20012@users.noreply.github.com>
2022-09-26 17:41:30 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -14,30 +14,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-from rest_framework.routers import DefaultRouter
-from tracker_api import views
-#from tracker_api import urls as tracker_urls
-from django.conf.urls import  include 
-#from .views import UserApiView
-#from todo_api import urls as todo_urls
 
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('tracker_api.urls')),
-    #path('api', UserApiView.as_view()),
-    #path('api-auth/', include('rest_framework.urls')),
-    #path('trackers_urls/', include(tracker_urls)),
-    #path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    
-    #path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include('tracker_api.urls'))
 ]
-# router = DefaultRouter()
-# router.register(r"user", views.MyAuthToken, basename="user")
-# urlpatterns += router.urls
